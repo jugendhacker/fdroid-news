a3f8032e9b1c1265a29465970a375f2d37c5b4f3
j.r <j.r@jugendhacker.de>
2022-06-06 14:25:15 +0200

    Use fdroid-news as default package in flake

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
     };
 
     packages.x86_64-linux.fdroid-news = pkgs.fdroid-news;
+    defaultPackage.x86_64-linux = pkgs.fdroid-news;
     nixosModules.fdroid-news = {
       imports = [ ./module.nix ];
       nixpkgs.overlays = [ self.overlays.default ];
