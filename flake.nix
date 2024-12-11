ba4526edb4a24d8c40a1037b23204a235717f86a
j.r <j.r@jugendhacker.de>
2024-12-11 11:54:51 +0100

    Upgrade to nixpkgs 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "fdroid-news XMPP bot";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
   };
 
   outputs = inputs@{ self, nixpkgs }:
@@ -17,12 +17,12 @@
     overlays.default = final: prev: {
       fdroid-news = let
         package-name = "fdroid-news";
-      in final.pkgs.buildGo121Module rec{
+      in final.pkgs.buildGoModule rec{
         name = "${package-name}";
         pname = "${package-name}";
         src = ./.;
         proxyVendor = true;
-        vendorHash = "sha256-+48Z30lSobB1LEngx4xzu2LPOvx1lXZfaTzHTdPy1kM=";
+        vendorHash = "sha256-idN6bF7OBlSyaESCAuQdpXC2HAJolRY3k8cqjzJfhUU=";
       };
     };
 
